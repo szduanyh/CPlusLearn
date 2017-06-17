9b17a672fc754846392387222a8cbc15b3401afe
nBread <duanyihua1234@hotmail.com>
2017-06-17 14:41:27 +0800

    优化

--- Queue/Queue/Queue.h
+++ Queue/Queue/Queue.h
@@ -8,29 +8,11 @@ class CQueue
 	int m_nCount;
 	int m_nHead, m_nTail;
 public:
-	CQueue(int nCount = 5){
-		m_pData = new DATA[nCount];
-		m_nHead = m_nTail = 0;
-		m_nCount = nCount;
-	}
+	CQueue(int nCount = 5);
 
-	void push(const DATA &data){
-		if (isFull())
-			return;
+	void push(const DATA &data);
 
-		m_pData[m_nTail] = data;
-		if (++m_nTail > m_nCount)
-			m_nTail = 0;
-	}
-
-	bool pop(DATA& data){
-		if (isEmpty())
-			return false;
-
-		data = m_pData[m_nHead];
-		if (++m_nHead > m_nCount)
-			m_nHead = 0;
-	}
+	bool pop(DATA& data);
 
 	bool isFull(){
 		return (m_nTail + 1) % m_nCount == m_nHead;
@@ -41,3 +23,28 @@ public:
 	}
 };
 
+template<typename DATA>
+CQueue<DATA>::CQueue(int nCount){
+	m_pData = new DATA[nCount];
+	m_nHead = m_nTail = 0;
+	m_nCount = nCount;
+}
+
+template<typename DATA>
+void CQueue<DATA>::push(const DATA &data){
+	if (isFull())
+		return;
+
+	m_pData[m_nTail] = data;
+	m_nTail = ++m_nTail%m_nCount;
+}
+
+template<typename DATA>
+bool CQueue<DATA>::pop(DATA& data){
+	if (isEmpty())
+		return false;
+
+	data = m_pData[m_nHead];
+	m_nHead = ++m_nHead%m_nCount;
+	return true;
+}
